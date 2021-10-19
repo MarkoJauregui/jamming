1148d052befff9198ce234c7af592107f204d98e
Maruko-coding <markojau@gmail.com>
2021-10-19 14:55:36 -0300

    first commit

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './Playlist.css';
+
+class Playlist extends Component {
+	render() {
+		return (
+			<div className="Playlist">
+				<input value="New Playlist" />
+				{/* <TrackList /> */}
+				<button className="Playlist-save">SAVE TO SPOTIFY</button>
+			</div>
+		);
+	}
+}
+
+export default Playlist;
